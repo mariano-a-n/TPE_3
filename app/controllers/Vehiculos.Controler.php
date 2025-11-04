6354cb47f805c3953ea96238f91baad66d61897f
Marcelo Gelato <gelatomarcelo77@gmail.com>
2025-11-03 21:37:53 -0300

    actualizado

--- app/controllers/Vehiculos.Controler.php
+++ app/controllers/Vehiculos.Controler.php
@@ -1,83 +1,154 @@
 <?php 
-include_once 'app/models/task.modelV.php';
+    include_once 'app/models/Vehiculos.model.php';
+    include_once 'app/models/Marcas.model.php';
 
-class VehiculosControler {
-    private $model;
+    class VehiculosControler {
+        private $model;
+        private $modelMarca;
 
-    function __construct(){
-        $this->model = new taskModelV();
-    }
-
-    // ==== HOME ====
-    // function showHomeUser($request) {
-    //     $modelos = $this->model->getCarModel();
-    //     $this->veiw->showTaksVehiculosUser($modelos, $request->user);
-    // }
-
-    function showHome($req, $res) {
-        $modelos = $this->model->getCarModel();
-
-        return $res->json($modelos);
-    }
-
-    // ==== VEHÍCULOS ====
-    function showCarBrandById($req, $res) {
-        $id = $req->params->id;
-        $vehiculo = $this->model-> getCarBrandById($id);
-
-        if (!$vehiculo) {
-            return $res->json("La marca con el id=$id no existe", 404);
+        function __construct(){
+            $this->model = new ModelVehiculos();
+            $this->modelMarca =  new ModelMarcas();
         }
-        return $res->json($vehiculo, 200);
-    }
 
-    function showCarDetails($id) {
-        $modelos = $this->model->getCarModel();
-        
-    }
+        // ==== HOME ====
+        // function showHomeUser($request) {
+        //     $modelos = $this->model->getCarModel();
+        //     $this->veiw->showTaksVehiculosUser($modelos, $request->user);
+        // }
 
-    function usedCars($id) {
-        $modelos = $this->model->getCarModel();
-        
-    }
+        function showHome($req, $res) {
+            if (isset($req->query->marca)) {
 
-    function newCars($id) {
-        $modelos = $this->model->getCarModel();
-        
-    }
+                $marca = (int)$req->query->marca;
 
-    // ==== ACCIONES ====
-    function sellCar($id) {
-        $this->model->updateCar($id);
-        header("Location: " . BASE_URL . "modelos");
-    }
+                $modelos = $this->model->getCarModel();
 
-    function eraseCar($id) {
-        $this->model->deleteCar($id);
-        header("Location: " . BASE_URL . "modelos");
-    }
+            }else{
+                $modelos = $this->model->getCarModel();
 
-    function addCarModel() {
-        if (isset($_POST['modelo'], $_POST['anio'], $_POST['km'], $_POST['precio'], $_POST['patente'], $_POST['marca'])
-            && !empty($_POST['marca']) && !empty($_POST['modelo']) && !empty($_POST['anio']) && !empty($_POST['precio'])) {
+                
+            }
+            return $res->json($modelos);
+        }
 
-            $modelo = trim($_POST['modelo']);
-            $anio = filter_var($_POST['anio'], FILTER_VALIDATE_INT);
-            $km = filter_var($_POST['km'], FILTER_VALIDATE_INT);
-            $precio = filter_var($_POST['precio'], FILTER_VALIDATE_FLOAT);
-            $patente = !empty($_POST['patente']) ? strtoupper(trim($_POST['patente'])) : null;
-            $es_nuevo = isset($_POST['es_nuevo']) ? 1 : 0;
-            $imagen = isset($_POST['imagen']) && filter_var($_POST['imagen'], FILTER_VALIDATE_URL) ? $_POST['imagen'] : null;
-            $vendido = isset($_POST['vendido']) ? 1 : 0;
-            $marca = trim($_POST['marca']);
-            $nacionalidad = $_POST['nacionalidad'] ?? null;
-            $anio_de_creacion = $_POST['anio_de_creacion'] ?? null;
+        // ==== VEHÍCULOS ====
+        function showCarBrandById($req, $res) {
+            $id = $req->params->id;
+            $vehiculo = $this->model-> getCarById($id);
 
-            $this->model->insertCar($modelo, $anio, $km, $precio, $patente, $es_nuevo, $imagen, $vendido, $marca, $nacionalidad, $anio_de_creacion);
+            if (!$vehiculo) {
+                return $res->json("el vehiculo con el id=$id no existe", 404);
+            }
+            return $res->json($vehiculo, 200);
+        }
+
+        function showCarDetails($id) {
+            $modelos = $this->model->getCarModel();
+            
+        }
+
+        function usedCars($id) {
+            $modelos = $this->model->getCarModel();
+            
+        }
+
+        function newCars($id) {
+            $modelos = $this->model->getCarModel();
+            
+        }
+
+        // ==== ACCIONES ====
+        function sellCar($id) {
+            $this->model->updateCar($id);
             header("Location: " . BASE_URL . "modelos");
-        } else {
-            $this->veiw->showError('Falta ingresar datos');
         }
+
+        function eraseCar($id) {
+            $this->model->deleteCar($id);
+            header("Location: " . BASE_URL . "modelos");
+        }
+
+
+        function addCarVehiculo($req, $res) {
+
+            //comprubo los datos
+
+            if (empty($req->body->id_marca) || !isset($req->body->id_marca)) {
+            return $res->json('Faltan datos', 400);
+            }
+            
+            if (empty($req->body->modelo) || !isset($req->body->modelo)) {
+            return $res->json('Faltan datos', 400);
+            }
+
+            if (empty($req->body->anio) || !isset($req->body->anio)) {
+            return $res->json('Faltan datos', 400);
+            }
+
+            if (empty($req->body->km) || !isset($req->body->km)) {
+            return $res->json('Faltan datos', 400);
+            }
+            
+            
+            if (empty($req->body->precio) || !isset($req->body->precio)) {
+            return $res->json('Faltan datos', 400);
+            }
+
+            if (empty($req->body->patente) || !isset($req->body->patente)) {
+            return $res->json('Faltan datos', 400);
+            }
+
+            if (empty($req->body->imagen) || !isset($req->body->imangen)) {
+            return $res->json('Faltan datos', 400);
+            }
+
+            $id_marca = (int) $req->body->id_marca;
+            $marca = $this->modelMarca->getCarBrandById($id_marca);
+
+            if (!$marca) {
+                return $res->json('la marca seleccionada no existe .', 404 );
+            }
+
+            // el trim quita los espacion antes y despues del txt.
+            $modelo = trim($req->body->modelo);
+            $anio = (int) $req->body->anio;
+            $km = (int) $req->body->km;
+            $precio = (float) $req->body->precio;
+
+            //strtoupper hece que todo el txt se vuelva en mayusculas
+            $patente = strtoupper(trim($req->body->patente));
+
+            $imagen = trim($req->body->imagen);
+
+            // Campos opcionales es nuevo por default en la tabla siempre es nuevo
+            if (isset($req->body->es_nuevo)) {
+                $es_nuevo = (int) $req->body->es_nuevo;
+            } else {
+                $es_nuevo = 0;
+            }
+
+            //comprovacion de logica  si es nuevo no puede tener mas de 0 km
+            if ($es_nuevo == 1 && $km > 0) {
+            return $res->json('error: Un vehículo nuevo no puede tener kilómetros.', 400);
+            }
+            //
+            if ($es_nuevo == 0 && $km <= 0) {
+            return $res->json('error: El vehiculo es usado pero tiene 0 km .', 400);
+            }
+
+            //comprobacion de la marca
+            
+
+            if (!empty($req->body->vendido) || ($req->body->vendido == 1)) {
+                return $res->json('error El vehículo no puede registrarse como vendido al crearse.', 400);
+            }
+            
+            $NuevoVehiculo = $this->model->insertCar($id_marca,$modelo,$anio,$km,$precio,$patente,$es_nuevo,$imagen);
+
+            return $res->json($NuevoVehiculo,201);
+        }
+
     }
-}
+    
 ?>
