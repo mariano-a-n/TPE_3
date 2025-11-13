ba9b6681c08409c0dbab42ed701362d0e0dfece4
Marcelo Gelato <gelatomarcelo77@gmail.com>
2025-11-12 23:12:43 -0300

    $router PATCH agregado

--- api_router.php
+++ api_router.php
@@ -13,20 +13,18 @@ $router = new Router();
 $router->addRoute('marcas',          'GET',      'MarcasControler',         'getBrands');
 $router->addRoute('marcas/:id',      'GET',      'MarcasControler',         'getBrandById');
 
-$router->addRoute('marcas/:id',      'DELETE',   'MarcasControler',         'removeBrend');
 $router->addRoute('marcas',          'POST',     'MarcasControler',         '');
 $router->addRoute('marcas/:id',      'PUT',      'MarcasControler',         '');
+$router->addRoute('marcas/:id',      'DELETE',   'MarcasControler',         'removeBrend');
 
 /// VEHICULOS /
 $router->addRoute('vehiculos',        'GET',      'VehiculosControler',     'showHome');
 $router->addRoute('vehiculos/:id',    'GET',      'VehiculosControler',     'showCarBrandById');
 
+$router->addRoute('vehiculos',        'POST',     'VehiculosControler',     'postCar');
+$router->addRoute('vehiculos/:id',    'PUT',      'VehiculosControler',     'putCar');
+$router->addRoute('vehiculos/:id',    'PATCH',    'VehiculosControler',     'patchCar');
 $router->addRoute('vehiculos/:id',    'DELETE',   'VehiculosControler',     'deleteCar');
-$router->addRoute('vehiculos',        'POST',     'VehiculosControler',     'addCarVehiculo');
-$router->addRoute('vehiculos/:id',    'PUT',      'VehiculosControler',     'refreshCar');
-
-
-
 
 
 /// OTROS
